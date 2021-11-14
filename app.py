46d045295183eb48c07dcdaf163faa7b7dabd62d
Alex <alex.osheter@gmail.com>
2021-11-14 22:17:15 +0200

    Major refactor -- needs to be tested

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from app import create_app, db
 from app.models import Experiment, Author, Material
 
-app = create_app()
+application = create_app()
 
 
-@app.shell_context_processor
+@application.shell_context_processor
 def make_shell_context():
     return {'db': db, 'Author': Author, 'Experiment': Experiment, 'Material': Material}
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
